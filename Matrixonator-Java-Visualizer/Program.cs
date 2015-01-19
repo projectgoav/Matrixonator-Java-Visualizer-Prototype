3b392b71683d13c2459d20241991cb7f1d03d24f
projectgoav <edm22@hotmail.co.uk>
2015-01-19 23:15:33 +0000

    Added basic connection open/read/close

---
+++ Matrixonator-Java-Visualizer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Matrixonator_Java_Visualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sk = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            while (sk.Connected == false)
+            {
+                Console.Write("Connecting...");
+                try
+                {
+                    sk.Connect("127.0.0.1", 9876);
+                }
+                catch { Console.WriteLine("REFUSED"); }
+
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Connected!");
+            Console.WriteLine("----------\n");
+
+            byte[] bytes = new byte[256];
+
+            while (sk.Connected == true)
+            {
+                try
+                {
+                    int i = sk.Receive(bytes);
+                    string Data = Encoding.UTF8.GetString(bytes);
+                    if ((Data == "") || (Data == null)) { Data = "NULL"; }
+                    Console.WriteLine(Data);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+                }
+            }
+        }
+    }
+}
